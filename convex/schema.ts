438cca589cf5192ad34e33b001c5f381332f0cc2
Frank Wei <frank@frankwei.xyz>
2026-02-14 01:00:08 +0800

    chore(db): setup convex

---
+++ convex/schema.ts
@@ -0,0 +1,23 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  /**
+   * message 请帮我参考 HybridEnvelope
+   */
+  message: defineTable({
+    v: v.number(),
+    alg: v.string(),
+    epk: v.string(),
+    nonce: v.string(),
+    ciphertext: v.string(),
+    recipients: v.array(
+      v.object({
+        kid: v.string(),
+        salt: v.string(),
+        wrapNonce: v.string(),
+        encCEK: v.string(),
+      }),
+    ),
+  }),
+});
